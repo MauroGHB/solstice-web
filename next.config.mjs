407a6c8214ba9edfb338f5896f125905791059b6
MauroGHB <pujolmauricio3@gmail.com>
2026-04-25 23:36:09 -0500

    fix next config y package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        forceSwcTransforms: false,
+    },
+}
 
-export default nextConfig;
+export default nextConfig
